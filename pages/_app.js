bf197dfa52fd49f4d416297017054bf2bdb192be
viver <viverbungag1@gmail.com>
2022-05-15 16:19:04 +0800

    Fixed sass problem when running dev

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+// import "../src/components/Atoms/Officer/Officer.module.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;

060786b07628941ae63cd2cbb59c7a2e9d4073b6
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2022-05-18 18:22:56 +0800

    Styles cleanup & reorganization
        > Moved variables.scss to ../styles/ folder
        > removed line height variables
        > removed duplicated font family properties

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import "../styles/globals.css";
-// import "../src/components/Atoms/Officer/Officer.module.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;

c019bc785f943db4b6500f54f9f91a39557c0f7c
viver <viverbungag1@gmail.com>
2022-05-18 11:58:46 +0800

    Finished Office component

--- src/components/ComponentIndex.js
+++ src/components/ComponentIndex.js
@@ -4,6 +4,7 @@ export { default as Button } from "./Atoms/Button/Button";
 
 /* MOLECULES */
 export { default as DepartmentNavigation } from "./Molecules/DepartmentNavigation/DepartmentNavigation";
+export { default as Office } from "./Molecules/Office/Office";
 
 /* ORGANISMS */
 export { default as Navigation } from "./Organisms/Navigation/Navigation";

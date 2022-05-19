0b1dd9f2dbcde78217922269549662b27700f2ca
Patrick <patmatayabas@gmail.com>
2022-05-19 21:35:18 +0800

    Started advocasix

---
+++ src/components/Atoms/Tabs/Tabs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tabs = ({category}) => {
+  return (
+    <div>{category}</div>
+  )
+}
+
+export default Tabs
--- src/components/ComponentIndex.js
+++ src/components/ComponentIndex.js
@@ -1,7 +1,7 @@
 /* ATOMS */
 export { default as Officer } from "./Atoms/Officer/Officer";
 export { default as Button } from "./Atoms/Button/Button";
-
+export { default as Tabs } from "./Atoms/Tabs/Tabs";
 /* MOLECULES */
 export { default as DepartmentNavigation } from "./Molecules/DepartmentNavigation/DepartmentNavigation";
 export { default as Office } from "./Molecules/Office/Office";

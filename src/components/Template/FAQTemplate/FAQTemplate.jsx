df7930577e4fb89741b268c0a3308f48ba2d330d
Corals <waynegabule@gmail.com>
2022-05-21 00:04:11 +0800

    FAQ added min-height

--- src/components/Template/FAQTemplate/FAQTemplate.jsx
+++ src/components/Template/FAQTemplate/FAQTemplate.jsx
@@ -52,6 +52,13 @@ const FAQTemplate = () => {
           <p className={styles["q-answer"]}>Answer number 4. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
         </div>
       </div>
+
+      <div className={styles["question"]}>
+        <div className={styles["q-container"]}>
+          <h2 className={styles["q-question"]}>Question Number 5?</h2>
+          <p className={styles["q-answer"]}>Answer number 5. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        </div>
+      </div>
       
       <Footer />
     </>

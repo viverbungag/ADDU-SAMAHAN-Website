2ba571a9fd0c84d0887de3e33da489d369505a98
Migs <miguelemmanueldailisan@gmail.com>
2022-05-21 15:56:57 +0800

    added initiative data index
    added initiative data index

--- src/data/dataIndex.js
+++ src/data/dataIndex.js
@@ -1,3 +1,4 @@
 export { default as scbData } from "./samahanCentralBoard.json";
 export { default as sdData } from "./samahanDepartments.json";
 export { default as advoData } from "./advocaSix.json";
+export { default as iniData} from "./initiativeData.json";

57d71a09b48ec61bf8b0b5a6c044f724f673c005
Patrick <patmatayabas@gmail.com>
2022-05-22 15:40:21 +0800

    Added reveal library and animation

--- src/components/Pages/LandingPage/LandingPage.jsx
+++ src/components/Pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { Footer, Navigation } from "../../ComponentIndex";
-
+import Fade from 'react-reveal/Fade';
 import styles from "./LandingPage.module.scss";
 
 const Landing = () => {
@@ -10,45 +10,53 @@ const Landing = () => {
       <Navigation transparent={true} />
       <section className={styles["landing"]}>
         <div className={styles["hero"] + " container"}>
-          <div className={styles["hero__slogan"]}>
-            <Image.default
-              src="https://firebasestorage.googleapis.com/v0/b/samahan-website.appspot.com/o/Images%2FHereAndNow.png?alt=media&token=42b3a96b-1104-444b-969a-7836d39253a8"
-              alt="Here and Now"
-              layout="fill"
-              objectFit="contain"
-              draggable="false"
-              className={styles["HereAndNow__img"]}
-              priority
-            />
-          </div>
+          <Fade top>
+            <div className={styles["hero__slogan"]}>
+              <Image.default
+                src="https://firebasestorage.googleapis.com/v0/b/samahan-website.appspot.com/o/Images%2FHereAndNow.png?alt=media&token=42b3a96b-1104-444b-969a-7836d39253a8"
+                alt="Here and Now"
+                layout="fill"
+                objectFit="contain"
+                draggable="false"
+                className={styles["HereAndNow__img"]}
+                priority
+              />
+            </div>
+          </Fade>
           <div className={styles["homepage"]}>
-            <div className={styles["homepage__define"]}>
-              <div className={styles["word"]}>
-                <h1>
-                  <strong>here</strong>
-                </h1>
-                <p>(adv.)</p>
+            <Fade left>
+              <div className={styles["homepage__define"]}>
+                <div className={styles["word"]}>
+                  <h1>
+                    <strong>here</strong>
+                  </h1>
+                  <p>(adv.)</p>
+                </div>
+                <p>to make known one{"'"}s existence</p>
               </div>
-              <p>to make known one{"'"}s existence</p>
-            </div>
-            <div className={styles["homepage__define"]}>
-              <div className={styles["word"]}>
-                <h1>
-                  <strong>and</strong>
-                </h1>
-                <p>(conj.)</p>
+            </Fade>
+            <Fade right>
+              <div className={styles["homepage__define"]}>
+                <div className={styles["word"]}>
+                  <h1>
+                    <strong>and</strong>
+                  </h1>
+                  <p>(conj.)</p>
+                </div>
+                <p>a connecting word</p>
               </div>
-              <p>a connecting word</p>
-            </div>
-            <div className={styles["homepage__define"]}>
-              <div className={styles["word"]}>
-                <h1>
-                  <strong>now</strong>
-                </h1>
-                <p>(adv.)</p>
+            </Fade>
+            <Fade left>
+              <div className={styles["homepage__define"]}>
+                <div className={styles["word"]}>
+                  <h1>
+                    <strong>now</strong>
+                  </h1>
+                  <p>(adv.)</p>
+                </div>
+                <p>at the present time or moment</p>
               </div>
-              <p>at the present time or moment</p>
-            </div>
+            </Fade>
           </div>
         </div>
       </section>

ac9f0d3943776c3dec6dc6f8f088958f31220b40
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2022-06-01 21:22:48 +0800

    Updated guidebook

--- src/components/Template/HelpPortalTemplate/HelpPortalTemplate.jsx
+++ src/components/Template/HelpPortalTemplate/HelpPortalTemplate.jsx
@@ -21,7 +21,7 @@ const HelpPortalTemplate = ({faqData, information}) => {
         <section className={styles["help-portal__guidebook"]}>
           <div className={styles["guidebook-wrapper"] + " container"}>
             <div className={styles["guidebook"]}>
-              <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/samahan-website.appspot.com/o/Images%2Fguidebook.png?alt=media&token=58e7af09-e17c-4f76-ad0e-6e911637a4f0" alt="SAMAHAN Guidebook" draggable={false}/>
+              <img loading="lazy" src="https://firebasestorage.googleapis.com/v0/b/samahan-website.appspot.com/o/Images%2Fguidebook.png?alt=media&token=5a320c7d-d038-47eb-b1db-bd3bf5d2e271" alt="SAMAHAN Guidebook" draggable={false}/>
             </div>
             <div className={styles["download-area"]}>
               <div className={styles["prompt"]}>

306369dfb3276cd92bb28f3b3692597bdca8eb3c
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2022-06-01 12:33:55 +0800

    Added loading progress bar from nprogress

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,34 +1,15 @@
 import Head from "next/head";
 import "../styles/globals.css";
-import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import "../styles/nprogress.css"
+import Router from "next/router";
+import nProgress from "nprogress";
+import { AnimateSharedLayout } from "framer-motion";
 import App from "next/app";
 
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <>
-//       <Head>
-//         <meta httpEquiv="X-UA-Compatible" content="IE=edge" key="httpEquiv" />
-//         <meta
-//           name="viewport"
-//           content="width=device-width, initial-scale=1.0"
-//           key="viewport"
-//         />
-
-//         {/* PRECONNECT TO GOOGLE FONT */}
-//         <link rel="preconnect" href="https://fonts.gstatic.com" />
-
-//         {/* TITLE AND ICON */}
-//         <title key="title">SAMAHAN</title>
-//       </Head>
-//       <AnimateSharedLayout type="crossfade">
-//         <AnimatePresence exitBeforeEnter>
-//           <Component {...pageProps} />
-//         </AnimatePresence>
-//       </AnimateSharedLayout>
-//       ;
-//     </>
-//   );
-// }
+/* LOADING PROGRESS BAR */
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 class MyApp extends App {
   render() {
